dc5091354e25c35aaf91ba7e6d2e25db11675ec5
Divyang <divyang@qdevtechnolab.com>
2024-07-06 16:47:48 +0530

    Chnages

--- Startpattern/Program.cs
+++ Startpattern/Program.cs
@@ -10,7 +10,7 @@ namespace Startpattern
     {
         static void Main(string[] args)
         {
-            int row = 5;
+            int row = 6;
             for (int i = 1; i <= row; i++)
             {
                for(int j = 1; j <= i; j++)
