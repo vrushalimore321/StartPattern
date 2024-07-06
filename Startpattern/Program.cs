e18f325e1dc1077a574f470113b179853d718dfa
Divyang <divyang@qdevtechnolab.com>
2024-07-06 16:42:31 +0530

    - Creted start pattern code

---
+++ Startpattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startpattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 5;
+            for (int i = 1; i <= row; i++)
+            {
+               for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+               Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
